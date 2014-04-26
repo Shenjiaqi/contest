f153ca8c84eed37e3ae9383da014e092029b223d
shenjiaqi <milkpowder2008@yeah.net>
2014-04-26 14:27:30 +0800

    not finished yet

---
+++ codeJam2014/Round1/a.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <fstream>
+#include <cassert>
+#include <set>
+#include <queue>
+#include <iostream>
+#include <utility>
+#include <stack>
+#include <complex>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <list>
+#include <functional>
+#include <cctype>
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef pair<int,int> ppi;
+typedef pair<ll,ll> ppl;
+typedef pair<double,double> ppd;
+#define PB push_back
+#define MP make_pair
+#define FIR first
+#define SEC second
+const int N = 157;
+const int L = 47;
+const int inf = 1e9;
+ll p[2][N];
+int main()
+{
+  int T;
+  scanf("%d", &T);
+  for( int i = 0; i < T; ++i )
+    {
+      int n, l;
+      scanf("%d%d", &n, &l);
+      for( int j = 0; j < 2; ++j )
+	for( int k = 0;
+	{
+	  char s[L];
+	  scanf("
+	}
+    }
+}
