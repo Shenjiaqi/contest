e3e8155e0357e851d0c4c7649b3f08415fd24096
shenjiaqi <1029393956@qq.com>
2014-04-27 00:45:47 +0800

    Round1 problem A got ac

--- codeJam2014/Round1/a.cpp
+++ codeJam2014/Round1/a.cpp
@@ -35,15 +35,54 @@ int main()
 {
   int T;
   scanf("%d", &T);
-  for( int i = 0; i < T; ++i )
+  for( int t = 0; t < T; ++t )
     {
       int n, l;
       scanf("%d%d", &n, &l);
+      vector<ll> a[2];
       for( int j = 0; j < 2; ++j )
-	for( int k = 0;
+	for( int k = 0; k < n; ++k )
+	  {
+	    char s[L];
+	    scanf("%s", s);
+	    ll tmp(0);
+	    for( int m = 0; m < l; ++m )
+	      {
+		tmp <<= 1;
+		if( s[m] == '1' )
+		  tmp |= 1;
+	      }
+	    a[j].push_back(tmp);
+	  }
+      vector<ll> b;
+      for( int i = 0; i < n; ++i )
+	for( int j = 0; j < n; ++j )
+	  b.push_back( a[0][i] ^ a[1][j] );
+      int ans = l + 1;
+      set<ll> d;
+      for( int i = 0; i < n; ++i )
+	d.insert( a[0][i] );
+      for( int i = 0; i < b.size(); ++i )
 	{
-	  char s[L];
-	  scanf("
+	  ll v = b[i];
+	  int num = __builtin_popcountll(v);
+	  if( num < ans )
+	    {
+	      vector<ll> c;
+	      int j = 0;
+	      for( ; j < n; ++j )
+		if( d.find( a[1][j] ^ v ) == d.end() )
+		  break;
+	      if( j >= n )
+		ans = num;
+	    }
 	}
+      printf("Case #%d: ", t + 1 );
+      if( ans <= l )
+      	printf("%d", ans );
+      else
+      	printf("NOT POSSIBLE");
+      printf("\n");
     }
+  return 0;
 }
